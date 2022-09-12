50b11f121c4ed7179b497048e91d84e4643d72f1
Basile Fraboni <basile.fraboni@insa-lyon.fr>
2022-09-12 15:18:10 +0200

    local variable types depend on template types for faster calculation (issue #5)

--- fast_gaussian_blur_template.h
+++ fast_gaussian_blur_template.h
@@ -69,12 +69,15 @@ enum BorderPolicy
 template<typename T, int C>
 void horizontal_blur_extend(const T * in, T * out, const int w, const int h, const int r)
 {
+    // change the local variable types depending on the template type for faster calculations
+    using calc_type = std::conditional_t<std::is_integral<T>::value, int, float>;
+
     float iarr = 1.f / (r+r+1);
     #pragma omp parallel for
     for(int i=0; i<h; i++) 
     {
         int ti = i*w, li = ti-r-1, ri = ti+r;   // current index, left index, right index
-        float fv[C], lv[C], acc[C];             // first value, last value, sliding accumulator
+        calc_type fv[C], lv[C], acc[C];         // first value, last value, sliding accumulator
 
         for(int ch = 0; ch < C; ++ch)
         {
@@ -116,11 +119,14 @@ void horizontal_blur_extend(const T * in, T * out, const int w, const int h, con
 template<typename T, int C>
 void horizontal_blur_kernel_crop(const T * in, T * out, const int w, const int h, const int r)
 {
+    // change the local variable types depending on the template type for faster calculations
+    using calc_type = std::conditional_t<std::is_integral<T>::value, int, float>;
+
     #pragma omp parallel for
     for(int i=0; i<h; i++) 
     {
         int ti = i*w, li = ti-r-1, ri = ti+r;   // current index, left index, right index
-        float acc[C];                           // sliding accumulator
+        calc_type acc[C];                       // sliding accumulator
 
         for(int ch = 0; ch < C; ++ch)
         {
